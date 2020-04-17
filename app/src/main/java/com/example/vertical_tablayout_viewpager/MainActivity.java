a451c65926363fec110a8213fa89c1913b1ffc07
liguohe <ysulgh@163.com>
2020-04-17 14:39:48 +0800

    first commit

--- app/src/main/java/com/example/vertical_tablayout_viewpager/MainActivity.java
+++ app/src/main/java/com/example/vertical_tablayout_viewpager/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.vertical_tablayout_viewpager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager2;
+    TabLayout mVerticalTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mVerticalTabLayout = findViewById(R.id.vertical_tablayout);
+        viewPager2 = findViewById(R.id.vertical_viewpager2);
+        viewPager2.setAdapter(new MyPagerAdapter());
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+        });
+        TabLayoutMediator mediator = new TabLayoutMediator(mVerticalTabLayout, viewPager2,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        tab.setText(String.valueOf(position));
+                    }
+                });
+
+        mediator.attach();
     }
+
+
 }
---
+++ app/src/main/java/com/example/vertical_tablayout_viewpager/MyPagerAdapter.java
@@ -0,0 +1,55 @@
+package com.example.vertical_tablayout_viewpager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * author:lgh on 2020-04-17 10:49
+ */
+public class MyPagerAdapter extends RecyclerView.Adapter<MyPagerAdapter.MyHolder> {
+
+    List<String> datas;
+
+    public MyPagerAdapter() {
+        datas = new ArrayList<>();
+        Collections.addAll(datas, "1", "2", "3", "4");
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+        holder.mTextView.setText(datas.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return datas.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder {
+        TextView mTextView;
+
+        MyHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextView = itemView.findViewById(R.id.item_title);
+        }
+    }
+
+}
